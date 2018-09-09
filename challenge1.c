73fd4b5d895062686558711e0ea296a96b9aff00
Haseeb Tora <torahaseeb@gmail.com>
2018-09-09 18:35:16 -0400

    change tab to spaces

--- challenge1.c
+++ challenge1.c
@@ -4,19 +4,19 @@
 
 int main()
 {
-	char *hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+  char *hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
-	char *answer = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-	
-	unsigned char *bytes = hexToBytes(hex);
-	char *b64 = bytesToB64(bytes);
+  char *answer = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+  
+  unsigned char *bytes = hexToBytes(hex);
+  char *b64 = bytesToB64(bytes);
 
-	if(strcmp(answer, b64) == 0)
-		printf("Success!\n");
-	
-	else
-		printf("Failure!\n");
+  if(strcmp(answer, b64) == 0)
+    printf("Success!\n");
+  
+  else
+    printf("Failure!\n");
 
-	free(bytes);
-	free(b64);
+  free(bytes);
+  free(b64);
 }
